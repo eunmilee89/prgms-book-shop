8e361ebc5cfc15892eec6304e1cc2d20c9687078
Eunmi Lee <eunoia0235@gmail.com>
2025-03-12 14:37:29 +0900

    feat: 도서 api 라우팅 및 컨트롤러 분리

--- routes/books.js
+++ routes/books.js
@@ -1,22 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { allBooks, bookDetail } = require('../controller/BookController');
 
 router.use(express.json());
 
-// 전체 도서 조회
-router.get('/', (req, res) => {
-  res.json('전체 도서 조회');
-});
-
-// 개별 도서 조회
-router.get('/:id', (req, res) => {
-  res.json('개별 도서 조회');
-});
-
-// 카테고리별 신간 도서 조회
-router.get('/', (req, res) => {
-  const { categoryId } = req.query;
-  res.json('카테고리별 신간 도서 조회');
-});
+router.get('/', allBooks); // 전체 도서 조회 & 카테고리별 신간 조회
+router.get('/:id', bookDetail); // 개별 도서 조회
 
 module.exports = router;
