64a76fe0c00bb777396f82a0768d32bb163e3de2
Eunmi Lee <eunoia0235@gmail.com>
2025-03-12 14:34:10 +0900

    feat: 카테고리 조회 api 추가

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ app.listen(process.env.PORT);
 
 const userRouter = require('./routes/users');
 const bookRouter = require('./routes/books');
+const categoryRouter = require('./routes/categories');
 const likeRouter = require('./routes/likes');
 const orderRouter = require('./routes/orders');
 const cartRouter = require('./routes/carts');
 
 app.use('/users', userRouter);
 app.use('/books', bookRouter);
+app.use('/categories', categoryRouter);
 app.use('/likes', likeRouter);
 app.use('/orders', orderRouter);
 app.use('/carts', cartRouter);
