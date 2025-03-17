3f0b54594257445d88b6fbcac0c1cdec7f9f06bd
Eunmi Lee <eunoia0235@gmail.com>
2025-03-17 20:41:24 +0900

    feat: 라우터와 컨트롤러 분리

--- routes/carts.js
+++ routes/carts.js
@@ -1,26 +1,15 @@
 const express = require('express');
+const {
+  addToCart,
+  getCartItems,
+  removeCartItems,
+} = require('../controller/CartController');
 const router = express.Router();
 
 router.use(express.json());
 
-// 장바구니 추가
-router.post('/', (req, res) => {
-  res.json('장바구니 추가');
-});
-
-// 장바구니 조회
-router.get('/', (req, res) => {
-  res.json('장바구니 조회');
-});
-
-// 장바구니 삭제
-router.delete('/:id', (req, res) => {
-  res.json('장바구니 삭제');
-});
-
-// 주문 예상 상품 목록 조회
-// router.get('/', (req, res) => {
-//   res.json('주문 예상 상품 목록 조회');
-// });
+router.post('/', addToCart); // 장바구니 추가
+router.get('/', getCartItems); // 장바구니 조회, 주문서 조회
+router.delete('/:id', removeCartItems); // 장바구니 삭제
 
 module.exports = router;
